1803243681a4995118fbc3c485fa95c7220b0709
leafhub <hnakayama@leafhub.me>
2020-09-24 21:54:20 +0900

    fix : ソース整形

--- src/app/payment/payment.component.ts
+++ src/app/payment/payment.component.ts
@@ -13,10 +13,11 @@ export class PaymentComponent implements OnInit {
   async ngOnInit() {
 
     const stripe = await loadStripe('pk_test_JYdiYD82MAZv5rHS6by6mKTn');
-    const elements = stripe.elements();
+    const elements = stripe.elements({});
+    const cardElement = elements.create('card', {});
 
-    const cardElement = elements.create('card');
     cardElement.mount('#card-element');
+
     cardElement.on('change', (event) => {
       const displayError = document.getElementById('card-errors');
       if (event.error){
@@ -27,9 +28,10 @@ export class PaymentComponent implements OnInit {
     });
 
     const paymentForm = document.getElementById('payment-form');
-    paymentForm.addEventListener('submit', event => {
+
+    paymentForm.addEventListener('submit', (event) => {
       event.preventDefault();
-      stripe.createToken(cardElement).then(result => {
+      stripe.createToken(cardElement).then((result) => {
         if (result.error) {
           console.log('Error creating payment method.');
           const errorElement = document.getElementById('card-errors');
